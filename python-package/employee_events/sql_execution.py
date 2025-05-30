65f94a164e7fcbff7bdcb17bbc5a04cc745c29e4
sherryhuang4 <sherryhuang1957@gmail.com>
2025-05-30 12:57:24 +0000

    Defined Mixin class

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
-
+db_path = Path(__file__).parent / 'employee_events.db'
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
@@ -16,14 +15,23 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(sql_query):
+        con = connect(db_path)
+        df = pd.read_sql_query(sql_query, con)
+        con.close()
+        return df
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+    def query(sql_query):
+        con = connect(db_path)
+        cur = con.cursor()
+        result = cur.execute(sql_query).fetchall()
+        con.close()
+        return result
     
 
  
